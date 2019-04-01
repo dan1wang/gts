1a24acb416eac8ac15375f82ad3c09084be8e53c
Daniel <daniel@MacBook-Pro.local>
2019-04-01 20:34:03 +0800

    still failing Azure DevOps :cry:

--- test/test-kitchen.ts
+++ test/test-kitchen.ts
@@ -115,7 +115,7 @@ describe('🚰 kitchen sink', () => {
   it('should use as a non-locally installed module', async () => {
     // Use from a directory different from where we have locally installed. This
     // simulates use as a globally installed module.
-    const GTS = `${stagingPath}/kitchen/node_modules/.bin/gts`;
+    const GTS = `${stagingPath}/kitchen/node_modules/.bin/gts.cmd`;
     const tmpDir = tmp.dirSync({ keep, unsafeCleanup: true });
     const opts = { cwd: `${tmpDir.name}/kitchen` };
 
